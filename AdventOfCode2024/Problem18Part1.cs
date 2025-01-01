088425224a690fe13c691734d30a7217ac7c63bb
MaxweIISmith <max-smith@mail.ru>
2025-01-01 18:32:59 +0300

    problem 18 part 1 solution

---
+++ AdventOfCode2024/Problem18Part1.cs
@@ -0,0 +1,75 @@
+﻿using AdventOfCode2024.Helpers;
+
+namespace AdventOfCode2024
+{
+    public static class Problem18Part1
+    {
+        public static void Solve()
+        {
+            var rowsCount = 71;
+            var columnsCount = 71;
+            var bytesCount = 1024;
+
+            var lines = new List<string>();
+
+            for (int i = 0; i < rowsCount; i++)
+            {
+                lines.Add(new string(Enumerable.Repeat('.', columnsCount).ToArray()));
+            }
+
+            var charMap = new CharMap(lines);
+
+            var reader = File.OpenText("input.txt");
+
+            for (int i = 0; i < bytesCount; i++)
+            {
+                var line = reader.ReadLine();
+                var parts = line.Split(',');
+
+                var x = Int32.Parse(parts[0]);
+                var y = Int32.Parse(parts[1]);
+
+                charMap[x, y] = '#';
+            }
+
+            int[,] scoreTable = new int[columnsCount, rowsCount];
+            for (int i = 0; i < columnsCount; i++)
+            {
+                for (int j = 0; j < rowsCount; j++)
+                {
+                    scoreTable[i, j] = int.MaxValue;
+                }
+            }
+
+            var start = new Vec2i();
+
+            scoreTable[start.X, start.Y] = 0;
+
+            FindLowestPath(charMap, start, scoreTable);
+
+            Console.WriteLine(scoreTable[columnsCount - 1, rowsCount - 1]);
+        }
+
+        private static void FindLowestPath(CharMap map, Vec2i pos, int[,] scoreTable)
+        {
+            foreach (var step in new[] { Vec2i.UnitX, -Vec2i.UnitX, Vec2i.UnitY, -Vec2i.UnitY })
+            {
+                var nextPos = pos + step;
+
+                if (nextPos.X < 0 || nextPos.X >= map.SizeX || nextPos.Y < 0 || nextPos.Y >= map.SizeY)
+                    continue;
+
+                if (map[nextPos] == '#')
+                    continue;
+
+                var score = scoreTable[pos.X, pos.Y] + 1;
+
+                if (score < scoreTable[nextPos.X, nextPos.Y])
+                {
+                    scoreTable[nextPos.X, nextPos.Y] = score;
+                    FindLowestPath(map, nextPos, scoreTable);
+                }
+            }
+        }
+    }
+}
