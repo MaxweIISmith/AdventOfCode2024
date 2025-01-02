e384cdab3ee30118d90c9175ef9722b46961e6ef
MaxweIISmith <max-smith@mail.ru>
2025-01-02 17:01:28 +0300

    problem 19 part 1 solution

---
+++ AdventOfCode2024/Problem19Part1.cs
@@ -0,0 +1,75 @@
+﻿namespace AdventOfCode2024
+{
+    public static class Problem19Part1
+    {
+        public static void Solve()
+        {
+            var reader = File.OpenText("input.txt");
+
+            var parts = reader.ReadLine().Split(",").Select(s => s.Trim()).ToList();
+
+            var parts2 = new List<string>();
+
+            foreach (var part in parts)
+            {
+                var tempParts = parts.ToList();
+                tempParts.Remove(part);
+
+                if (!CanCombineStringFromParts(part, tempParts))
+                {
+                    parts2.Add(part);
+                }
+            }
+
+            int count = 0;
+            while (!reader.EndOfStream)
+            {
+                var str = reader.ReadLine();
+
+                if (String.IsNullOrEmpty(str))
+                    continue;
+
+                if (CanCombineStringFromParts(str, parts2))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+
+        private static bool CanCombineStringFromParts(string str, List<string> parts, int index = 0)
+        {
+            foreach (var part in parts)
+            {
+                if (str.Length - index < part.Length)
+                    continue;
+
+                bool isEqual = true;
+                for (int i = 0; i < part.Length; i++)
+                {
+                    if (str[index + i] != part[i])
+                    {
+                        isEqual = false;
+                        break;
+                    }
+                }
+
+                if (!isEqual)
+                    continue;
+
+                var newIndex = index + part.Length;
+
+                if (newIndex == str.Length)
+                    return true;
+
+                if (CanCombineStringFromParts(str, parts, newIndex))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -8,7 +8,7 @@ namespace AdventOfCode2024
 
         private static void Main(string[] args)
         {
-            Problem18Part2.Solve();
+            Problem19Part1.Solve();
 
 #if DEBUG
             Console.Write("Press any key to continue...");
